8afbc35c332cff7efde3401115750f2bfde130b4
Eka Robyanto <robyantoeka@gmail.com>
2023-02-24 20:14:10 +0700

    update medicine post on obat.ts

--- src/pages/api/obat.ts
+++ src/pages/api/obat.ts
@@ -2,7 +2,6 @@ import prisma from "@/utils/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
 interface Data {
-  id: number;
   name: string;
   desc: string;
   activeIngredient: string;
@@ -23,10 +22,13 @@ export default async function handler(
     }
   } else if (req.method === "POST") {
     try {
+      const getallMed: Data[] = await prisma.medicine.findMany();
       const msg: Data = req.body;
-      const allMed: Data[] = await prisma.medicine.findMany();
-      if(allMed.includes(msg))return res.status(401).json({'detail':'data already exist'})
-        const record = await prisma.medicine.create({
+      const allMed = getallMed.filter((med)=> med.name === msg.name)
+      
+      if(allMed.length>0)return res.status(401).json({'detail':'data with that name already exist'})
+        
+      const record = await prisma.medicine.create({
           data: {
             name: msg.name,
             desc: msg.desc,
