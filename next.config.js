33a0e767ceb72aff75d7db86427b8bb93a193e75
MichaelJuannn <juan600d@gmail.com>
2023-05-10 18:45:40 +0700

    cors allow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
+};
